30f8c317b42d7f11899d15574589a9e0cead5d59
Simon Grigenas <grigenas@outlook.com>
2022-11-25 18:52:22 -0500

    Completed initial development environment setup

---
+++ client/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+import App from "./components/App.jsx";
+import styles from './app.css';
+import { BrowserRouter } from 'react-router-dom';
+
+const rootDiv = document.getElementById('root');
+const root = createRoot(rootDiv);
+
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
