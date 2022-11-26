017ae1b62975acac9925caa8fd8965423c774e45
c-catanese <111301202+c-catanese@users.noreply.github.com>
2022-11-25 19:22:50 -0500

    commiting changes for skeleton of controllers and routers

---
+++ server/controllers/userController.js
@@ -0,0 +1,11 @@
+const db = require('./models/userModel.js');
+
+userController = {};
+
+userController.verifyLogin = (req, res, next) => {
+
+};
+
+userController.createUser = (req, res, next) => {};
+
+userController.getInfo = (req, res, next) => {};
---
+++ server/routes/userRouter.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/')//add controller shit 
+
+router.post('/signup')//add controller shit. 
+
+router.get('/mainpage')//add controller shit 
