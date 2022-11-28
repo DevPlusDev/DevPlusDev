c634d72784b94d47a0d25d36a5d077fa2c7b9bb7
Jackson Kalmbach <jacksonrkalmbach@gmail.com>
2022-11-28 10:37:55 -0500

    working dual call version

--- client/components/settings.js
+++ client/components/settings.js
@@ -1,7 +1,7 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "a0c5b211bfaa4092972eb225f76e13a7";
-const token = "007eJxTYHDlr7N8tbP7/fnG82yHDgRpiT6buvWmYc99nz/zbF9L9PUoMCQaJJsmGRkaJqUlJpoYWBpZmhulJhkZmaaZm6UaGieay+U0JjcEMjJ4ft3MysgAgSA+C0NuYmYeAwMAyCYg6Q==";
+const token = "007eJxTYLATXtETfidrzYmTXR8/JdyKf7amdL6n7Lzc/emvNiesWxqlwJBokGyaZGRomJSWmGhiYGlkaW6UmmRkZJpmbpZqaJxo/vZ8S3JDICPD9YvCTIwMEAjiszDkJmbmMTAAAEHjIuw=";
 
 export const config = {
     mode: "rtc",
